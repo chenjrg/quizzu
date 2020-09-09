a0f37464e2933afdd7b0c84d0516a5e42993e2ac
chenjrg <121990240+chenjrg@users.noreply.github.com>
2020-09-09 19:54:20 +0000

    R CMD check

--- Ryo Tracker/Ryo Tracker/Exchange.swift
+++ Ryo Tracker/Ryo Tracker/Exchange.swift
@@ -2,3 +2,25 @@
 //
 //  File.swift
 //  Ryo Tracker
+//
+//  Created by Will Ravenscroft on 19/06/2018.
+//  Copyright © 2018 Zypher DX. All rights reserved.
+//
+
+import Foundation
+
+public enum exchangeVal: Int {
+    case tradeogre = 0
+    case maplechange = 1
+}
+
+public class Exchange {
+    
+    private var URLS = ["https://tradeogre.com/api/v1","https://maplechange.com//api/v2"]
+    private var tradeBinURLS = ["https://tradeogre.com/api/v1/history/BTC-RYO"]
+    private var tickerURLS = ["https://tradeogre.com/api/v1/ticker/BTC-RYO","https://maplechange.com:443//api/v2/tickers/ryobtc.json"]
+    private var exchangeNames = ["TRADEOGRE","MapleChange"]
+    private var exchange: Int
+    
+    init() {
+        exchange = exchangeVal.tradeogre.rawValue
