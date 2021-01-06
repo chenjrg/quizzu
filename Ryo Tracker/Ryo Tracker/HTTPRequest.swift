d53d25c5ea71e884d4d61608e3454bd73c85c25a
chenjrg <121990240+chenjrg@users.noreply.github.com>
2021-01-06 21:56:06 +0000

    Adding test file

--- Ryo Tracker/Ryo Tracker/HTTPRequest.swift
+++ Ryo Tracker/Ryo Tracker/HTTPRequest.swift
@@ -32,4 +32,5 @@ class HTTPRequest {
     
     static func requestTradeBin(exchange: Int, url: String, tradeBinResponse: @escaping (Any) -> Void){
         Alamofire.request(url).responseString { response in
-  
+            
+     
