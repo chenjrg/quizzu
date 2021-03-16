652bcb39d8cdafb36f3427b39cf8a341b19b5260
chenjrg <121990240+chenjrg@users.noreply.github.com>
2021-03-16 13:42:25 +0000

    deploy: kit-sdq/SDQ-Commons@fb3ef14b861294823ad1e3000affb6c44c7c049e

--- Ryo Tracker/Ryo Tracker/HTTPRequest.swift
+++ Ryo Tracker/Ryo Tracker/HTTPRequest.swift
@@ -38,4 +38,4 @@ class HTTPRequest {
             let jsonData = jsonString!.data(using: .utf8)!
             if exchange == exchangeVal.tradeogre.rawValue {
                 
-                let tradeBin = try! decoder
+                let tradeBin = try! decoder.decode([TOTrade
