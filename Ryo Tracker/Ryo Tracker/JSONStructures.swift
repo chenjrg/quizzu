245169efb828e3d8fe27918225cbc8424188ec84
chenjrg <121990240+chenjrg@users.noreply.github.com>
2021-10-25 13:18:46 +0000

    Ajout des fichiers de langue française site.

--- Ryo Tracker/Ryo Tracker/JSONStructures.swift
+++ Ryo Tracker/Ryo Tracker/JSONStructures.swift
@@ -14,4 +14,5 @@ struct TOTicker : Codable {
     public var price : String
     public var high : String
     public var low : String
-    public var volume
+    public var volume : String
+    public va
