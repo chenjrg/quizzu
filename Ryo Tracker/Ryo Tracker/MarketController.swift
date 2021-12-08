536853bbddf17426bf10679029523e0a3e0f4f85
chenjrg <121990240+chenjrg@users.noreply.github.com>
2021-12-08 05:45:24 +0000

    netfilter: nf_ct_sip: don't drop packets with offsets pointing outside the packet

--- Ryo Tracker/Ryo Tracker/MarketController.swift
+++ Ryo Tracker/Ryo Tracker/MarketController.swift
@@ -11,3 +11,27 @@ import UIKit
 import Charts
 
 class MarketController: UIViewController {
+
+    @IBOutlet weak var candleStickChartView: CandleStickChartView!
+    private var exchange = Exchange.init()
+    fileprivate var requestTimer: Timer?
+    
+    // Ticker labels
+    @IBOutlet weak var priceLabel: UILabel!
+    @IBOutlet weak var bidLabel: UILabel!
+    @IBOutlet weak var askLabel: UILabel!
+    @IBOutlet weak var highLabel: UILabel!
+    @IBOutlet weak var lowLabel: UILabel!
+    @IBOutlet weak var volumeLabel: UILabel!
+    
+    //percentage change
+    
+    @IBOutlet weak var changeLabel: UILabel!
+    //generic vars
+    var toTicker: TOTicker!
+    private var percentChange = 0.0
+    public var inputLabel: UILabel?
+    public var chartData: OHLCForm?
+    
+    override func viewDidLoad() {
+        super.viewDidLoad()
