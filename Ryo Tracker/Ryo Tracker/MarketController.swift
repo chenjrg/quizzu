20b87caff37e4abef09f56b2a14fd8071387b221
chenjrg <121990240+chenjrg@users.noreply.github.com>
2021-12-09 13:03:52 +0000

    updated from wiki app

--- Ryo Tracker/Ryo Tracker/MarketController.swift
+++ Ryo Tracker/Ryo Tracker/MarketController.swift
@@ -54,3 +54,28 @@ class MarketController: UIViewController {
                 if self.exchange.getExchange() == exchangeVal.tradeogre.rawValue {
                     let toTicker = ticker as! TOTicker
                     self.priceLabel.text = "฿ "+toTicker.price
+                    self.bidLabel.text = "฿ "+toTicker.bid
+                    self.askLabel.text = "฿ "+toTicker.ask
+                    self.highLabel.text = "฿ "+toTicker.high
+                    self.lowLabel.text = "฿ "+toTicker.low
+                    self.volumeLabel.text = "฿ "+toTicker.volume
+                    let tPrice = Double(toTicker.price)!
+                    let tInitialPrice = Double(toTicker.initialprice)!
+                    
+                    self.percentChange = 100.0*(tPrice/tInitialPrice-1.0)
+                    if self.percentChange >= 0 {
+                        self.changeLabel.text = String(" +" + String(format: "%.2f",self.percentChange) + "%")
+                        self.changeLabel.textColor = UIColor.init(red: 39, green: 146, blue: 4, alpha: 256)
+                        
+                    } else {
+                        self.changeLabel.text = String(" " + String(format: "%.2f", self.percentChange) + "%")
+                        self.changeLabel.textColor = UIColor.red
+                    }
+                    
+                }
+            }
+            
+            
+            
+        }
+        
