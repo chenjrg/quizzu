d368eba1f497476d593c7c663c7b42a2cd8bd83b
chenjrg <121990240+chenjrg@users.noreply.github.com>
2021-12-15 08:50:28 +0000

    missing changes

--- Ryo Tracker/Ryo Tracker/MarketDataHandling.swift
+++ Ryo Tracker/Ryo Tracker/MarketDataHandling.swift
@@ -2,3 +2,24 @@
 //
 //  MarketDataHandling.swift
 //  Ryo Tracker
+//
+//  Created by Will Ravenscroft on 26/06/2018.
+//  Copyright © 2018 Zypher DX. All rights reserved.
+//
+
+import Foundation
+
+struct OHLCForm {
+    public var length: Int
+    public var date: [Date]
+    public var open: [Double]
+    public var high: [Double]
+    public var low: [Double]
+    public var close: [Double]
+    
+    init(hourlyBucket: HourlyTimeBucket) {
+        length = hourlyBucket.length
+        date = Array(repeating: Date.init(), count: length)
+        open = Array(repeating: 0.0, count: length)
+        high = Array(repeating: 0.0, count: length)
+        low = Array(repeating: 0.0, count: length)
