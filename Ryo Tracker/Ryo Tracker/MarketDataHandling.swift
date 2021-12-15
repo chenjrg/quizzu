d85faf59d61c3d82cbae412ca007a310230c94bb
chenjrg <121990240+chenjrg@users.noreply.github.com>
2021-12-15 19:00:12 +0000

    debug msgs

--- Ryo Tracker/Ryo Tracker/MarketDataHandling.swift
+++ Ryo Tracker/Ryo Tracker/MarketDataHandling.swift
@@ -23,3 +23,25 @@ struct OHLCForm {
         open = Array(repeating: 0.0, count: length)
         high = Array(repeating: 0.0, count: length)
         low = Array(repeating: 0.0, count: length)
+        close = Array(repeating: 0.0, count: length)
+        for i in 0...length-1 {
+            date[i] = hourlyBucket.bucketStartDate[i]
+            open[i] = hourlyBucket.pricesBin[i][0]
+            high[i] = hourlyBucket.pricesBin[i].max()!
+            low[i] = hourlyBucket.pricesBin[i].min()!
+            close[i] = hourlyBucket.pricesBin[i][hourlyBucket.pricesBin[i].count-1]
+        }
+    }
+}
+
+struct HourlyTimeBucket {
+    public var length : Int
+    public var bucketHour : [Int]
+    public var bucketStartDate : [Date]
+    public var bucketEndDate : [Date]
+    public var pricesBin : [[Double]]
+    
+    init (tradeBin: [TOTradeBin], size: Int) {
+        length = size
+        bucketHour = Array(repeating: 0, count: size)
+        bucketStartDate = Array(repeating: roundDownStart(date: Date.init()), count: size)
