fdc1186e75f3d95aa68817470229f37f64f446b3
chenjrg <121990240+chenjrg@users.noreply.github.com>
2022-02-11 22:53:27 +0000

    [DOCU] German:

--- Ryo Tracker/Ryo Tracker/NetworkController.swift
+++ Ryo Tracker/Ryo Tracker/NetworkController.swift
@@ -32,4 +32,5 @@ class NetworkController: UIViewController {
     // MARK: - Navigation
 
     // In a storyboard-based application, you will often want to do a little preparation before navigation
-    override func prepare(for segue: UIStoryboardSeg
+    override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
+ 
