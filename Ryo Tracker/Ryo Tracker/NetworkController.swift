62772c371de92cbc8b749c949d5a976c9436fee4
chenjrg <121990240+chenjrg@users.noreply.github.com>
2022-03-09 14:02:58 +0000

    [FROM NICOLAS@BNF]

--- Ryo Tracker/Ryo Tracker/NetworkController.swift
+++ Ryo Tracker/Ryo Tracker/NetworkController.swift
@@ -43,3 +43,6 @@ class NetworkController: UIViewController {
             newsController.chartData = chartData
         }
     }
+    
+
+}
---
+++ Ryo Tracker/Ryo Tracker/NetworkDataHandling.swift
@@ -0,0 +1,15 @@
+
+//
+//  NetworkDataHandling.swift
+//  Ryo Tracker
+//
+//  Created by Will Ravenscroft on 01/07/2018.
+//  Copyright © 2018 Zypher DX. All rights reserved.
+//
+
+import Foundation
+
+struct StatisticsFormat {
+    public var difficulty : Double
+    public var hash : String
+    public var height : Int
