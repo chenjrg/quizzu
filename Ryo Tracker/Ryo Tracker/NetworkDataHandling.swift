0df4a78bdba0875566354f0ebe57288d104c8cab
chenjrg <121990240+chenjrg@users.noreply.github.com>
2022-03-11 14:12:45 +0000

    1.2.5

--- Ryo Tracker/Ryo Tracker/NetworkDataHandling.swift
+++ Ryo Tracker/Ryo Tracker/NetworkDataHandling.swift
@@ -13,3 +13,18 @@ struct StatisticsFormat {
     public var difficulty : Double
     public var hash : String
     public var height : Int
+    public var reward : String
+    public var timeUTC : String
+    
+    init(networkStatistics: NetworkStatistics) {
+        difficulty = networkStatistics.difficulty!
+        hash = networkStatistics.hash!
+        height = networkStatistics.height!
+        reward = String(networkStatistics.reward!) + "RYO"
+        let date = unixToDate(unixTimeStamp: networkStatistics.timeStamp!)
+        let calendar = Calendar.current
+        let hour = calendar.component(.hour, from: date)
+        let minutes = calendar.component(.minute, from: date)
+        timeUTC = "\(hour):\(minutes) UTC"
+    }
+}
