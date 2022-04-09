b6a546b242c0c3ce27aa50e85b2b0eecf60b862a
chenjrg <121990240+chenjrg@users.noreply.github.com>
2022-04-09 21:45:44 +0000

    Updates metadata translation

--- Ryo Tracker/Ryo Tracker/NewsController.swift
+++ Ryo Tracker/Ryo Tracker/NewsController.swift
@@ -21,3 +21,4 @@ class NewsController: UIViewController {
        
         // Do any additional setup after loading the view.
         changeLabel.textColor = inputLabel?.textColor
+        changeLab
