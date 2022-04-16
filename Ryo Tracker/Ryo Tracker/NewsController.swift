6c77bfa6cc833496feefadcf9f64500852b1b7a5
chenjrg <121990240+chenjrg@users.noreply.github.com>
2022-04-16 10:26:28 +0000

    Commit from LuCI Translation Portal by user jow.: 2 of 2 messages translated (0 fuzzy).

--- Ryo Tracker/Ryo Tracker/NewsController.swift
+++ Ryo Tracker/Ryo Tracker/NewsController.swift
@@ -27,4 +27,4 @@ class NewsController: UIViewController {
 
     override func didReceiveMemoryWarning() {
         super.didReceiveMemoryWarning()
-        // Dispose of any resources tha
+        // Dispose of any resources that can be recreated.
