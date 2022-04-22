cf7240d6c1b65c53e1127978fe377258f280b1ca
chenjrg <121990240+chenjrg@users.noreply.github.com>
2022-04-22 14:37:19 +0000

    bumped up the version

--- Ryo Tracker/Ryo Tracker/NewsController.swift
+++ Ryo Tracker/Ryo Tracker/NewsController.swift
@@ -32,4 +32,4 @@ class NewsController: UIViewController {
     
     override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
         if let marketController = segue.destination as? MarketController {
-            marketController
+            marketController.inputLabel = change
