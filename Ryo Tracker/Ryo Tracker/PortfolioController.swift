64878729c19c93652d8cfc64b122d393d5bad735
chenjrg <121990240+chenjrg@users.noreply.github.com>
2022-09-29 13:50:16 +0000

    Fixed stuff.

--- Ryo Tracker/Ryo Tracker/PortfolioController.swift
+++ Ryo Tracker/Ryo Tracker/PortfolioController.swift
@@ -27,4 +27,4 @@ class PortfolioController: UIViewController {
 
     // In a storyboard-based application, you will often want to do a little preparation before navigation
     override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
-        // Get the new view controller u
+        // Get the new view controller using segue.destina
