b0079a1072c2f2fa21ecdf52c37a6bf4fec7f530
chenjrg <121990240+chenjrg@users.noreply.github.com>
2022-10-05 10:14:32 +0000

    rm cruft

--- Ryo Tracker/Ryo Tracker/UtilityFunctions.swift
+++ Ryo Tracker/Ryo Tracker/UtilityFunctions.swift
@@ -1,3 +1,28 @@
 
 //
 //  UtilityFunctions.swift
+//  Ryo Tracker
+//
+//  Created by Will Ravenscroft on 28/06/2018.
+//  Copyright © 2018 Zypher DX. All rights reserved.
+//
+
+import Foundation
+
+func roundUpEnd(date: Date) -> Date {
+    var newDate = date
+    let minute = Calendar.current.component(.minute, from: date)
+    newDate = Calendar.current.date(byAdding: .minute, value: 60-minute, to: newDate)!
+    let second = Calendar.current.component(.second, from: date)
+    newDate = Calendar.current.date(byAdding: .second, value: -second, to: newDate)!
+    return newDate
+}
+
+func roundDownStart(date: Date) -> Date {
+    var newDate = date
+    let minute = Calendar.current.component(.minute, from: date)
+    newDate = Calendar.current.date(byAdding: .minute, value: (-minute), to: newDate)!
+    let second = Calendar.current.component(.second, from: date)
+    newDate = Calendar.current.date(byAdding: .second, value: -second, to: newDate)!
+    return newDate
+}
