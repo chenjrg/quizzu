773bc7c486075ea726035f57c55c41e5273d1d8b
chenjrg <121990240+chenjrg@users.noreply.github.com>
2022-10-05 07:29:44 +0000

    Fixed a warning

--- Ryo Tracker/Ryo Tracker/PortfolioController.swift
+++ Ryo Tracker/Ryo Tracker/PortfolioController.swift
@@ -29,3 +29,7 @@ class PortfolioController: UIViewController {
     override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
         // Get the new view controller using segue.destinationViewController.
         // Pass the selected object to the new view controller.
+    }
+    */
+
+}
