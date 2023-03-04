fe652c88e9d0f4dc5ebffc2b3c6b29cfc933205f
chenjrg <121990240+chenjrg@users.noreply.github.com>
2023-03-04 16:56:41 +0000

    update the log.

--- Ryo Tracker/Ryo TrackerTests/Ryo_TrackerTests.swift
+++ Ryo Tracker/Ryo TrackerTests/Ryo_TrackerTests.swift
@@ -25,3 +25,13 @@ class Ryo_TrackerTests: XCTestCase {
     func testExample() {
         // This is an example of a functional test case.
         // Use XCTAssert and related functions to verify your tests produce the correct results.
+    }
+    
+    func testPerformanceExample() {
+        // This is an example of a performance test case.
+        self.measure {
+            // Put the code you want to measure the time of here.
+        }
+    }
+    
+}
