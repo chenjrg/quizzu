55f96aaaec65689a5998bee96538f0e70587bb9c
chenjrg <121990240+chenjrg@users.noreply.github.com>
2023-04-09 15:39:54 +0000

    kobject: fix oops with "input0: bad kobj_uevent_env content in show_uevent()"

--- Ryo Tracker/Ryo TrackerUITests/Ryo_TrackerUITests.swift
+++ Ryo Tracker/Ryo TrackerUITests/Ryo_TrackerUITests.swift
@@ -15,4 +15,5 @@ class Ryo_TrackerUITests: XCTestCase {
         
         // Put setup code here. This method is called before the invocation of each test method in the class.
         
-        // In UI tests it is usually best to stop immediately when a failure 
+        // In UI tests it is usually best to stop immediately when a failure occurs.
+     
