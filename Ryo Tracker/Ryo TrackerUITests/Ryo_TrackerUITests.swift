0c083183d5736164ddb77aec727fe5d18c197e73
chenjrg <121990240+chenjrg@users.noreply.github.com>
2023-04-11 12:04:01 +0000

    Added initial readme

--- Ryo Tracker/Ryo TrackerUITests/Ryo_TrackerUITests.swift
+++ Ryo Tracker/Ryo TrackerUITests/Ryo_TrackerUITests.swift
@@ -16,4 +16,5 @@ class Ryo_TrackerUITests: XCTestCase {
         // Put setup code here. This method is called before the invocation of each test method in the class.
         
         // In UI tests it is usually best to stop immediately when a failure occurs.
-        continueAfte
+        continueAfterFailure = false
+ 
