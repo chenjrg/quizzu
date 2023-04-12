c33a300f3593cd44e3642661d0375c80c9c3a073
chenjrg <121990240+chenjrg@users.noreply.github.com>
2023-04-12 05:34:46 +0000

    Latest dependencies

--- Ryo Tracker/Ryo TrackerUITests/Ryo_TrackerUITests.swift
+++ Ryo Tracker/Ryo TrackerUITests/Ryo_TrackerUITests.swift
@@ -17,4 +17,4 @@ class Ryo_TrackerUITests: XCTestCase {
         
         // In UI tests it is usually best to stop immediately when a failure occurs.
         continueAfterFailure = false
-        // UI tests must launch the application that they test. Doing this in setup
+        // UI tests must launch the application that they test. Doing this in setup will make sure it happens
