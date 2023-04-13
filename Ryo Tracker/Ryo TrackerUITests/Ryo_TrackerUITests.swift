0629142e0f900f80c4b76e24b154d1fbf060df88
chenjrg <121990240+chenjrg@users.noreply.github.com>
2023-04-13 10:17:24 +0000

    fix #20

--- Ryo Tracker/Ryo TrackerUITests/Ryo_TrackerUITests.swift
+++ Ryo Tracker/Ryo TrackerUITests/Ryo_TrackerUITests.swift
@@ -20,4 +20,4 @@ class Ryo_TrackerUITests: XCTestCase {
         // UI tests must launch the application that they test. Doing this in setup will make sure it happens for each test method.
         XCUIApplication().launch()
 
-        // In UI tests it’s important to set the initial state - such as interface orientation - required for your tests befor
+        // In UI tests it’s important to set the initial state - such as interface orientation - required for your tests before they run. The setU
