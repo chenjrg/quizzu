2e5b38a362a638eb46407998c517c2a515115070
chenjrg <121990240+chenjrg@users.noreply.github.com>
2023-04-13 20:12:44 +0000

    testing dict

--- Ryo Tracker/Ryo TrackerUITests/Ryo_TrackerUITests.swift
+++ Ryo Tracker/Ryo TrackerUITests/Ryo_TrackerUITests.swift
@@ -23,4 +23,4 @@ class Ryo_TrackerUITests: XCTestCase {
         // In UI tests it’s important to set the initial state - such as interface orientation - required for your tests before they run. The setUp method is a good place to do this.
     }
     
-    ove
+    override func tearDown(
