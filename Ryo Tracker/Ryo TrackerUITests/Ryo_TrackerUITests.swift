a956873a25e5c8aac3cdb502060331c44fab9910
chenjrg <121990240+chenjrg@users.noreply.github.com>
2023-04-13 19:10:47 +0000

    Fix list formatting

--- Ryo Tracker/Ryo TrackerUITests/Ryo_TrackerUITests.swift
+++ Ryo Tracker/Ryo TrackerUITests/Ryo_TrackerUITests.swift
@@ -20,4 +20,7 @@ class Ryo_TrackerUITests: XCTestCase {
         // UI tests must launch the application that they test. Doing this in setup will make sure it happens for each test method.
         XCUIApplication().launch()
 
-        // In UI tests it’s important to set the initial state - such as interface orientation - required for your tests before they run. The setUp method is a good place to do 
+        // In UI tests it’s important to set the initial state - such as interface orientation - required for your tests before they run. The setUp method is a good place to do this.
+    }
+    
+    ove
