5eacca8405f8ba165926113d9dd86faf971c9ce4
chenjrg <121990240+chenjrg@users.noreply.github.com>
2023-04-15 20:39:21 +0000

    Updated file

--- Ryo Tracker/Ryo TrackerUITests/Ryo_TrackerUITests.swift
+++ Ryo Tracker/Ryo TrackerUITests/Ryo_TrackerUITests.swift
@@ -25,4 +25,5 @@ class Ryo_TrackerUITests: XCTestCase {
     
     override func tearDown() {
         // Put teardown code here. This method is called after the invocation of each test method in the class.
-      
+        super.tearDown()
+   
