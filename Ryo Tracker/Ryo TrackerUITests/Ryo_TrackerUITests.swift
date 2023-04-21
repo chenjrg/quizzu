e9afb883764951170ad557ecc5cec7edf6909e0a
chenjrg <121990240+chenjrg@users.noreply.github.com>
2023-04-21 15:06:10 +0000

    Version bump to 0.2.10

--- Ryo Tracker/Ryo TrackerUITests/Ryo_TrackerUITests.swift
+++ Ryo Tracker/Ryo TrackerUITests/Ryo_TrackerUITests.swift
@@ -31,3 +31,6 @@ class Ryo_TrackerUITests: XCTestCase {
     func testExample() {
         // Use recording to get started writing UI tests.
         // Use XCTAssert and related functions to verify your tests produce the correct results.
+    }
+    
+}
